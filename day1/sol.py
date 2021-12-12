f2385cd35b1e91eda58e2b2e72edafdf4f080065
Nathan Simpson <phinate@Nathans-MacBook-Air.local>
2021-12-12 12:56:57 +0100

    more sols

--- day1/sol.py
+++ day1/sol.py
@@ -1,14 +1,14 @@
-from typing import Counter
-
-Counter
-
-
 def main():
     with open("input") as f:
         data = [int(x) for x in f.read().split()]
 
-    lst = [one for one, two in zip(data, data[1:]) if one - two > 0]
+    lst = [one for one, two in zip(data, data[1:]) if two - one > 0]
     print(f"answer: {len(lst)} instances of increasing")
+
+    # part 2
+    window = list(zip(data, data[1:], data[2:]))
+    lst = [sum(two) - sum(one) > 0 for one, two in zip(window, window[1:])]
+    print(f"sliding window comparisons: {sum(lst)} increasing instances")
 
 
 if __name__ == "__main__":
--- day3/sol.py
+++ day3/sol.py
@@ -6,14 +6,12 @@ from jax import jit
 def gamma_calc(data: jnp.ndarray) -> tuple[float, float]:
     added = jnp.sum(data, axis=0)
     cutoff = data.shape[1] // 2
-    print(cutoff)
     return jnp.where(added > cutoff, 1, 0)
 
 
 def main():
     with open("input") as f:
         data = jnp.asarray([[int(y) for y in x.strip()] for x in f.readlines()])
-    print(data)
     print(gamma_calc(data))
 
 
