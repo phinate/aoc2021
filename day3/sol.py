cf5c722389e0bc85ea43234b8f93b4602243c2c5
Nathan Simpson <phinate@Nathans-MacBook-Air.local>
2021-12-13 14:18:12 +0100

    sols

--- day3/sol.py
+++ day3/sol.py
@@ -2,16 +2,20 @@ import jax.numpy as jnp
 from jax import jit
 
 
-def gamma_calc(data: jnp.ndarray) -> tuple[float, float]:
+@jit
+def gamma_calc(data: jnp.ndarray) -> tuple[jnp.ndarray, jnp.ndarray]:
     added = jnp.sum(data, axis=0)
     cutoff = data.shape[0] // 2
-    return jnp.where(added > cutoff, 1, 0)
+    return jnp.where(added > cutoff, 1, 0), jnp.where(added > cutoff, 0, 1)
 
 
 def main():
     with open("input") as f:
         data = jnp.asarray([[int(y) for y in x.strip()] for x in f.readlines()])
-    print(str(gamma_calc(data).tolist()))
+    g, e = gamma_calc(data)
+    gamma = int("".join([str(x) for x in g]), 2)
+    epsilon = int("".join([str(x) for x in e]), 2)
+    print(gamma * epsilon)
 
 
 if __name__ == "__main__":
